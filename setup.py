738765d875d216d41f6547913c06c0a8828a61e0
Pam Suebvisai <sinapam@proteus-tech.com>
2012-04-23 19:57:17 +0700

    try setting up setup.py to include templates

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='restful',
-      version='1.2',
+      version='1.2.1',
       description='RESTful for Django',
       author='Proteus Technologies',
       author_email='team@proteus-tech.com',
       url='http://proteus-tech.com',
       install_requires=['django>=1.2', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3'],
-      package_data={'templates': []},
+      package_data={'restful': ['templates/restful/*.html']},
       packages=['restful'],
      )
