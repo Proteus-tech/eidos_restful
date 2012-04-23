d242c02ca818075fae2ae4de1d36af6a4cd6f19e
Pam Suebvisai <sinapam@proteus-tech.com>
2012-04-23 20:15:50 +0700

    change django version

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(name='restful',
       author='Proteus Technologies',
       author_email='team@proteus-tech.com',
       url='http://proteus-tech.com',
-      install_requires=['django>=1.2', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3'],
+      # django is Eidos current version of django
+      install_requires=['django==1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3'],
       package_data={'restful': ['templates/restful/*.html']},
       packages=['restful'],
      )
