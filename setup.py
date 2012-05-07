56f3d2da9a12dec92ba108be8977071a1c2b4838
Pam Suebvisai <sinapam@proteus-tech.com>
2012-05-08 00:37:50 +0700

    try to fix setup file

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     # django is Eidos current version of django
-    dependency_links = ['http://github.com/scoursen/django-softdelete/tarball/master#egg=django-softdelete-0.3.4customdownload',],
-    setup_requires = ['django-softdelete==0.3.4customdownload'],
+    dependency_links = ['http://github.com/scoursen/django-softdelete/tarball/master#egg=django-softdelete-0.3.4',],
+    setup_requires = ['django-softdelete==0.3.4'],
     install_requires=['django==1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3'],
     package_data={'restful': ['templates/restful/*.html']},
     packages=['restful'],
