2505d1aa563aefd3a6c39a2317d28798dc7b37e3
Pam Suebvisai <sinapam@proteus-tech.com>
2012-05-08 00:47:47 +0700

    try to fix setup file

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from distutils.core import setup
 
 setup(
     name='restful',
-    version='1.2.7',
+    version='1.2.8',
     description='RESTful for Django',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     # django is Eidos current version of django
     dependency_links = ['http://github.com/scoursen/django-softdelete/tarball/master#egg=django-softdelete-0.3.4',],
-    setup_requires = ['django-softdelete==0.3.4'],
-    install_requires=['django==1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3'],
+    install_requires=['django==1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3', 'django-softdelete==0.3.4'],
     package_data={'restful': ['templates/restful/*.html']},
     packages=['restful'],
 )
