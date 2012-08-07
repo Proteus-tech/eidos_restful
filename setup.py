d3bf5fc1c6b9487063dde718e38823c08897e49f
Euam <euam@cottoncandy.proteus-tech.com>
2012-08-07 17:44:51 +0700

    update version and install requires version for django

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup
 
 setup(
     name='restful',
-    version='1.3.0',
+    version='1.3.1',
     description='RESTful for Django',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     # django is Eidos current version of django
-    install_requires=['django==1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3', 'django-softdelete==0.3.4'],
+    install_requires=['django>=1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3', 'django-softdelete==0.3.4'],
     package_data={'restful': ['templates/restful/*.html']},
     packages=['restful', 'restful.middleware'],
 )
