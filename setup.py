66c6a32bd51957e603c49d24bf2d32f37b51e6c3
Pam Suebvisai <sinapam@proteus-tech.com>
2012-09-03 21:40:01 +0700

    removed djangorestframework requirements again

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     # django is Eidos current version of django
-    install_requires=['django>=1.3.1', 'djangorestframework==0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3', 'django-softdelete==0.3.4'],
+    install_requires=['django>=1.3.1', 'django-serene>=0.0.5', 'django-piston==0.2.3', 'django-softdelete==0.3.4'],
     package_data={'restful': ['templates/restful/*.html']},
     packages=['restful', 'restful.middleware'],
 )
